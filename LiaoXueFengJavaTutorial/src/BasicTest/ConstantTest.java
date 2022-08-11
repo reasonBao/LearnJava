fb30d5d30ea5ff316a14f4094d3b26c54cb77304
baoruishen <1490323807@qq.com>
2022-08-11 17:11:52 +0800

    å­跟廖雪峰æ老师学真的真的比看书有意思多，并且学得更快，å能学进去，所以，找到一个好å教程是很重要的

--- LiaoXueFengJavaTutorial/src/BasicTest/ConstantTest.java
+++ LiaoXueFengJavaTutorial/src/BasicTest/ConstantTest.java
@@ -1,9 +1,29 @@
 package BasicTest;
 
+import java.util.Arrays;
+
 public class ConstantTest {
     public static void main(String[] args) {
-        int a = 123;
-        int b = 1231;
-        System.out.println(a ^ b);
+
+
+        Person p = new Person();
+        String[] fullName = new String[] {"bao", "ruisehn"};
+        p.setName(fullName);
+        System.out.println(p.getName());
+
+        fullName[0] = "ZHANG";
+        System.out.println(p.getName());
+    }
+}
+
+class Person {
+    private String[] name;
+
+    public String getName() {
+        return this.name[0] + " " + this.name[1];
+    }
+
+    public void setName(String[] name) {
+        this.name = name;
     }
 }
